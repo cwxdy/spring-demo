90719eecbf5facece366e148dda034d198b44a77
ychang1 <44756649@qq.com>
2019-03-15 18:41:02 +0800

    6

--- src/main/java/com/example/demo/base/StartupRunner.java
+++ src/main/java/com/example/demo/base/StartupRunner.java
@@ -1,13 +1,17 @@
 package com.example.demo.base;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StartupRunner implements ApplicationRunner {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("###启动成功###");
+        logger.info("###启动成功###");
     }
 }
