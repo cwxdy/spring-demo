25139bec9ef6f6b9a9f919c4475217067d1f27a0
CHANG Yu <yu.a.chang@capgeminigemini.com>
2019-04-22 14:30:49 +0800

    x

--- src/main/java/com/example/demo/base/BaseController.java
+++ src/main/java/com/example/demo/base/BaseController.java
@@ -1,4 +1,6 @@
 package com.example.demo.base;
+import com.example.demo.dto.GeneralResponseDto;
+import org.apache.shiro.authz.UnauthorizedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -10,7 +12,20 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class BaseController {
     public static Logger logger = LoggerFactory.getLogger(BaseController.class);
-
+    /**
+     * 所有异常统一格式返回
+     * @param except
+     * @return
+     */
+    @ExceptionHandler(Exception.class)
+    public GeneralResponseDto runtimeExceptionHandler(Exception except) {
+        except.printStackTrace();
+        if(except instanceof UnauthorizedException){
+            return GeneralResponseDto.addError(-1, "权限不足");
+        }else{
+            return GeneralResponseDto.addError(-1, except.getMessage());
+        }
+    }
     /**
      * 这里只是示例，将来shiro配置那里配置页面地址
      * @return
